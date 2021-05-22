6d0922e829e8b357cda392f53d165abc7dcb194a
Bogdan Pastiu <bogdan.pastiu@gmail.com>
2021-05-22 20:24:51 +0300

    Another one

--- proiect.R
+++ proiect.R
@@ -3,6 +3,9 @@ library(modelr)
 library(scatterplot3d)
 library(readr,dplyr)
 library(ggplot2)
+library(ISLR)
+library(caret)
+library(rsample)
 
 Analysis <- read_csv("/Users/bogdanpastiu/Desktop/bigdata/dataset/breast_cancer_data.csv")
 # TODO data cleanup
@@ -15,6 +18,16 @@ Analysis %>%
 
 Analysis %>%
   ggplot(aes(mean_radius, mean_perimeter, color=diagnosis)) + geom_point() + geom_smooth() # graph displaying correclation between radius and perimeter
+
+split=sample.split(Analysis$diagnosis,SplitRatio=0.65)
+Analysis<-Analysis[-33]
+training_set<-subset(Analysis,split==T)
+View(training_set)
+test_set<-subset(Analysis,split==F)
+View(test_set)
+
+bc_reg<-glm(data = Analysis, diagnosis ~ mean_radius + mean_area + mean_perimeter + mean_smoothness, family = binomial)
+summary(bc_reg)
 
 
 
